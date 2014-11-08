65751981eaa295a8ae6da121284da062625e03ba
Zheng-Li <zl714@nyu.edu>
2014-11-08 15:20:02 -0500

    Chapter 4 Question 4

--- chapter_4/TreeNode.java
+++ chapter_4/TreeNode.java
@@ -1,5 +1,8 @@
 package chapter_4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 
 public class TreeNode {
 	private int value;
@@ -26,7 +29,38 @@ public class TreeNode {
 		right = node;
 	}
 	
-	public void showTreeNode(TreeNode node) {
-		//Unfinished to show the layout of the binary tree
+	public int getValue() {
+		return value;
+	}
+	
+	public ArrayList<LinkedList<TreeNode>> showTree(TreeNode root) {
+		ArrayList<LinkedList<TreeNode>> tree = new ArrayList<LinkedList<TreeNode>>();
+		createTreeList(tree, 0, root);
+		for(LinkedList<TreeNode> list: tree) {
+			for(TreeNode tn : list) {
+				System.out.print(tn.getValue() + " ");
+			}
+			System.out.println();
+		} 
+		return tree;
+	}
+	
+	//Show result by LinkedList of each depth
+	public void createTreeList(ArrayList<LinkedList<TreeNode>> tree, 
+			int level, TreeNode root) {
+		if(root == null) return;
+		LinkedList<TreeNode> list;
+	
+		if(tree.size() == level) {
+			//When the level is not in the arraylist
+			list = new LinkedList<TreeNode>();
+			tree.add(list);
+		} else {
+			list = tree.get(level);
+		}
+		list.add(root);
+
+		createTreeList(tree, level+1, root.left);
+		createTreeList(tree, level+1, root.right);
 	}
 }
